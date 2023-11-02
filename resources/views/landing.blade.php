61c0f27e8a25a2a3ee4a7f4baead841c3867f09c
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2023-11-02 19:18:49 +0700

    fix boy10

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -87,7 +87,7 @@
                         <p class="wow fadeInLeft">
                            Sistem Informasi Data Mikro yang dimiliki oleh Desa Sawentar Kecamatan Kanigoro Kabupaten Blitar
                         </p>
-                        <a class="wow fadeInDown btn btn-light border btn-md" href="https://www.youtube.com/watch?v=l_nwLjT8Vzg">PROFILE SITAKRO</a>
+                        <a class="wow fadeInDown btn btn-light border btn-md popup-youtube light video-play-button video-inline" href="https://www.youtube.com/watch?v=l_nwLjT8Vzg">PROFILE SITAKRO</a>
                         {{-- <a href="https://www.youtube.com/watch?v=l_nwLjT8Vzg" class="popup-youtube light video-play-button video-inline">
                             <i class="fa fa-play"></i>
                         </a> --}}
