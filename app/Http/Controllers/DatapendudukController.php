75ddd8cdf7bbe7bda5366e143889e3acacb55ec8
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2023-11-03 10:39:59 +0700

    fix boy10

--- app/Http/Controllers/DatapendudukController.php
+++ app/Http/Controllers/DatapendudukController.php
@@ -77,7 +77,8 @@ class DatapendudukController extends Controller
     $file->move('file_siswa', $nama_file);
 
     // import data
-    Excel::import(new Importdatapenduduk, public_path('sdgsdesa/public/file_siswa/' . $nama_file));
+    Excel::import(new Importdatapenduduk, storage_path('app/public/sdgsdesa/public/file_siswa/' . $nama_file));
+
     
 
 
--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('sdgsdesa/public/file_siswa/'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
