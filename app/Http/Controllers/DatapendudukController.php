fa4fabf30b26748eeb79292265cb665d8e83e779
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2023-11-03 07:51:07 +0700

    fix boy10

--- app/Http/Controllers/DatapendudukController.php
+++ app/Http/Controllers/DatapendudukController.php
@@ -77,7 +77,7 @@ class DatapendudukController extends Controller
     $file->move('file_siswa', $nama_file);
 
     // import data
-    Excel::import(new Importdatapenduduk, public_path('/file_siswa/' . $nama_file));
+    Excel::import(new Importdatapenduduk, '/home/sitakroc/public_html/sdgsdesa/public' . $nama_file);
 
     // notifikasi dengan session
 
