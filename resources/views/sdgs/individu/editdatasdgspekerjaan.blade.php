f52fa12e63f0e0eae1b4aa733cf07bfc9ac0baf6
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2023-11-05 14:19:48 +0700

    fix boy9

--- resources/views/sdgs/individu/editdatasdgspekerjaan.blade.php
+++ resources/views/sdgs/individu/editdatasdgspekerjaan.blade.php
@@ -55,7 +55,7 @@
                                     <select class="form-control @error('valPekerjaanutama') is-invalid @enderror" id="valPekerjaanutama" name="valPekerjaanutama">
                                         <option value="0" disabled selected>--Pilih Pekerjaan--</option>
                                         @foreach ($pekerjaan as $item)
-                                        <option value="{{ $item->id }}" {{ $datap->agama_id == $item->id ? 'selected' : '' }} >{{ $item->nama }}</option>
+                                        <option value="{{ $datap->agama_id ?? '' }}" {{ $datap->agama_id == $item->id ? 'selected' : '' }} >{{ $item->nama }}</option>
                                     @endforeach
                                     </select>
                                     
