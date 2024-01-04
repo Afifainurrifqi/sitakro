1b84d1258695947dd5a0fd3012f0ea2850ae50e8
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2024-01-04 16:45:09 +0700

    FIX BOYS

--- app/Models/datapenduduk.php
+++ app/Models/datapenduduk.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class datapenduduk extends Model
+class Datapenduduk extends Model
 
 
 
