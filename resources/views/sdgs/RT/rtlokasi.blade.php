2551caf013f1b7cde688b56bf90b8d556dd6dbd1
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2024-01-11 09:47:51 +0700

    FIX BOYS

--- resources/views/sdgs/RT/rtlokasi.blade.php
+++ resources/views/sdgs/RT/rtlokasi.blade.php
@@ -75,8 +75,8 @@
             </div>
         </div>
     </div>
-
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
     <script>
         var $ = jQuery.noConflict();
         $(function() {
