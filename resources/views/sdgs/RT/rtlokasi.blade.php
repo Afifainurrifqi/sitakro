ddc48f10da3338d930939bb9c2ed48d19d4097eb
Afif Ainur Rifqi <afifainurrifqi@gmail.com>
2024-01-12 07:52:51 +0700

    FIX BOYS

--- resources/views/sdgs/RT/rtlokasi.blade.php
+++ resources/views/sdgs/RT/rtlokasi.blade.php
@@ -84,7 +84,7 @@
                 processing: true,
                 serverSide: true,
                 scrollX: true,
-                ajax: '{{ url("/rtlokasi/json") }}',
+                ajax: '/rtlokasi/json',
                 columns: [{
                         data: 'action',
                         name: 'action',
