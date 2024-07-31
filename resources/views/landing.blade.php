5fee462362c302d9e7129083a15cc570c7fc20d5
Afifainurrifqi <afifainurrifqi@gmail.com>
2024-07-31 22:59:30 +0700

    bismillah

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -58,7 +58,7 @@
             <div class="row">
                 <div class="double-items text-light">
                     <div class="info col-md-7">
-                        <img src="assets2/img/logosi.png" class="wow slideInLeft"  style="width: 50%">
+                        <img src="{{ 'assets2/img/logosi.png' }}" class="wow slideInLeft"  style="width: 50%">
                         <h2 class="wow slideInLeft">SITAKRO SAWENTAR </h2>
                         <p class="wow fadeInLeft">
                             Sistem Informasi Data Mikro yang dimiliki oleh Desa Sawentar Kecamatan Kanigoro Kabupaten
